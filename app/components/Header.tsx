de0ab63137215be2a280eddf01428356e897d76d
n1zenTablet <torresizen070204@gmail.com>
2026-09-05 20:39:11 +0800

    change border color to slate-100

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,7 +2,7 @@ import NavBar from "./NavBar";
 
 export default function Header() {
     return(
-        <header className="border rounded-lg py-2 px-6 my-2 mx-auto flex justify-center items-center"> 
+        <header className="border border-slate-100 rounded-lg py-2 px-6 my-2 mx-auto flex justify-center items-center"> 
             {/* Title */}
             <div className="p-0 m-0 flex justify-center items-center">
                 <h1 className="font-bold text-xl">n1zen.dev</h1>
