bddbb1e8ff24ce12ca9cc0ca07e77f256f72e7e8
n1zenTablet <torresizen070204@gmail.com>
2026-09-06 18:34:43 +0800

    added className as prop to Card.tsx

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,6 +1,11 @@
-export default function Card(children: React.ReactNode) {
+interface CardProps {
+    children: React.ReactNode;
+    className?: string;
+}
+
+export default function Card({children, className}: CardProps) {
     return(
-	<div className="border border-slate-400 rounded-lg">
+	<div className={`border border-slate-400 rounded-lg ${className ?? ""}`}>
 	    {children}
 	</div>
     );
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,18 +4,16 @@ import Card from "./Card";
 
 export default function Header() {
     return(
-	<Card>
-	    <header className="max-w-6xl w-full py-2 px-6 my-8 mx-auto flex justify-between items-center"> 
-		{/* Title */}
-		<div className="p-0 flex">
-		    <h1 className="font-bold text-xl font-heading">n1zen.dev</h1>
-		</div>
-		{/* Navigation and Other Actions */}
-		<div className="p-0 flex">
-		    <NavBar />
-		    <ActionBar />
-		</div>
-	    </header>
+	<Card className="max-w-6xl w-full py-2 px-6 my-8 mx-auto flex justify-between items-center">
+	    {/* Title */ }
+	    <div className="p-0 flex">
+		<h1 className="font-bold text-xl font-heading">n1zen.dev</h1>
+	    </div>
+	    {/* Navigation and Other Actions */}
+	    <div className="p-0 flex">
+		<NavBar />
+		<ActionBar />
+	    </div>
 	</Card>
     );
 }
