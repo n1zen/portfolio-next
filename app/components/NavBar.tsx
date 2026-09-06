8a3d91f7b9c2bd1c7e79d090916b5b4146bbdd33
n1zenTablet <torresizen070204@gmail.com>
2026-09-06 15:37:51 +0800

    Remove all space-x-n classNames

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -27,7 +27,7 @@ export default function NavBar() {
     ];
 
     return(
-        <div className="flex justify-center items-center space-x-10 p-0 m-auto">
+        <div className="flex justify-center items-center p-0 m-auto">
             {Routes.map((routeProp) => (
                 <NavLink key={routeProp.routeId} href={routeProp.url}>
                     {routeProp.name}
