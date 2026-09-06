a4a63f9078c9a38dc4cd0f0f803157fccfb9c68b
n1zenTablet <torresizen070204@gmail.com>
2026-09-06 15:23:36 +0800

    enclose title in a div

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,7 +5,9 @@ export default function Header() {
     return(
         <header className="border border-slate-400 rounded-lg py-2 px-6 my-2 mx-auto flex justify-between items-center"> 
             {/* Title */}
-            <h1 className="font-bold text-xl font-heading">n1zen.dev</h1>
+	    <div className="m-auto p-0 flex">
+		<h1 className="font-bold text-xl font-heading">n1zen.dev</h1>
+	    </div>
 	    {/* Navigation and Other Actions */}
 	    <div className="m-auto p-0 flex">
 		<NavBar />
