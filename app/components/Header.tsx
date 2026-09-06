cdb4117056ea5a55de727e03790ee178ee54622a
n1zenTablet <torresizen070204@gmail.com>
2026-09-06 15:53:41 +0800

    remove m-auto classNames

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,11 +5,11 @@ export default function Header() {
     return(
         <header className="border border-slate-400 rounded-lg py-2 px-6 my-2 mx-auto flex justify-between items-center"> 
             {/* Title */}
-	    <div className="m-auto p-0 flex">
+	    <div className="p-0 flex">
 		<h1 className="font-bold text-xl font-heading">n1zen.dev</h1>
 	    </div>
 	    {/* Navigation and Other Actions */}
-	    <div className="m-auto p-0 flex">
+	    <div className="p-0 flex">
 		<NavBar />
 		<ActionBar />
 	    </div>
