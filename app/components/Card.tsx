696da560fd693528425dd62feebcbf9264fb1032
n1zenTablet <torresizen070204@gmail.com>
2026-09-06 18:23:53 +0800

    Create Card component for Card styling

---
+++ app/components/Card.tsx
@@ -0,0 +1,7 @@
+export default function Card(children: React.ReactNode) {
+    return(
+	<div className="border border-slate-400 rounded-lg">
+	    {children}
+	</div>
+    );
+}
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,18 +1,21 @@
 import NavBar from "./NavBar";
 import ActionBar from "./ActionBar";
+import Card from "./Card";
 
 export default function Header() {
     return(
-        <header className="max-w-6xl w-full border border-slate-400 rounded-lg py-2 px-6 my-8 mx-auto flex justify-between items-center"> 
-            {/* Title */}
-	    <div className="p-0 flex">
-		<h1 className="font-bold text-xl font-heading">n1zen.dev</h1>
-	    </div>
-	    {/* Navigation and Other Actions */}
-	    <div className="p-0 flex">
-		<NavBar />
-		<ActionBar />
-	    </div>
-        </header>
+	<Card>
+	    <header className="max-w-6xl w-full py-2 px-6 my-8 mx-auto flex justify-between items-center"> 
+		{/* Title */}
+		<div className="p-0 flex">
+		    <h1 className="font-bold text-xl font-heading">n1zen.dev</h1>
+		</div>
+		{/* Navigation and Other Actions */}
+		<div className="p-0 flex">
+		    <NavBar />
+		    <ActionBar />
+		</div>
+	    </header>
+	</Card>
     );
 }
