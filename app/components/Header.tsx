8121a24cfc0fb4690cd64e0d85f30264b3e9e229
n1zenTablet <torresizen070204@gmail.com>
2026-09-06 14:59:37 +0800

    change layout and use justify-between instead of space-x-n and justify-center

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,17 +3,14 @@ import ActionBar from "./ActionBar";
 
 export default function Header() {
     return(
-        <header className="border border-slate-400 rounded-lg py-2 px-6 my-2 space-x-64 mx-auto flex justify-center items-center"> 
+        <header className="border border-slate-400 rounded-lg py-2 px-6 my-2 mx-auto flex justify-between items-center"> 
             {/* Title */}
             <h1 className="font-bold text-xl font-heading">n1zen.dev</h1>
-            {/* Navigation */}
-            <div>
-                <NavBar/>
-            </div>
-            {/* Other Actions */}
-            <div>
-                <ActionBar />
-            </div>
+	    {/* Navigation and Other Actions */}
+	    <div className="m-0 p-0 flex justify-between">
+		<NavBar />
+		<ActionBar />
+	    </div>
         </header>
     );
 }
