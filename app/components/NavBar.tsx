c9be1fcc68cdcc5fbaf4712fa45b4b0e03b61354
n1zenTablet <torresizen070204@gmail.com>
2026-09-06 14:17:16 +0800

    uhh what?

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -5,19 +5,17 @@ import Link from "next/link";
 interface NavLinkProps {
     href: string;
     children: React.ReactNode; 
-} 
-
-interface route { 
+} interface route { 
     routeId: number; 
     name: string; 
     url: string; 
 } 
-function NavLink({href, children}: NavLinkProps) 
-{ return( 
-    <Link href={href} className="relative text-gray-700 hover:text-black font-subheading group transition-colors duration-200">
-        {children}
-        <span className="absolute left-1/2 bottom-0 w-0 h-[2px] bg-black transition-all duration-300 origin-center -translate-x-1/2 group-hover:w-full"/>
-    </Link>
+function NavLink({href, children}: NavLinkProps) { 
+    return( 
+	<Link href={href} className="relative text-gray-700 hover:text-black font-subheading group transition-colors duration-200">
+            {children}
+            <span className="absolute left-1/2 bottom-0 w-0 h-[2px] bg-black transition-all duration-300 origin-center -translate-x-1/2 group-hover:w-full"/>
+        </Link>
     );
 }
 
