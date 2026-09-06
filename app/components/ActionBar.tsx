5acdc389b8e8015369a9639d643047c27c7a9d71
n1zenTablet <torresizen070204@gmail.com>
2026-09-06 17:49:13 +0800

    add links to socials in ActionBar icons

--- app/components/ActionBar.tsx
+++ app/components/ActionBar.tsx
@@ -4,12 +4,12 @@ import { IoLogoLinkedin } from "react-icons/io";
 export default function ActionBar() {
     return(
         <div className="flex space-x-2 mx-4">
-            <div className="bg-slate-300 p-1.5 rounded">
-                <IoLogoGithub fontSize={"1.25em"}/>
-            </div>
-            <div className="bg-slate-300 p-1.5 rounded">
-                <IoLogoLinkedin fontSize={"1.25em"}/>
-            </div>
+	    <a href="https://github.com/n1zen" className="bg-slate-300 p-1.5 rounded">
+		<IoLogoGithub fontSize={"1.25em"}/>
+	    </a>
+	    <a href="https://www.linkedin.com/in/louie-izen-torres-4557243ab?utm_source=share_via&utm_content=profile&utm_medium=member_android" className="bg-slate-300 p-1.5 rounded">
+		<IoLogoLinkedIn fontSize={"1.25em"}/>
+	    </a>
         </div>
     );
 }
