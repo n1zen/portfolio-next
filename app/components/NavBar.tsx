e99e35f48be331251db074c3acc573b07f9e70e1
n1zenTablet <torresizen070204@gmail.com>
2026-09-06 14:13:10 +0800

    testing on nvim

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,21 +4,20 @@ import Link from "next/link";
 
 interface NavLinkProps {
     href: string;
-    children: React.ReactNode;
-}
+    children: React.ReactNode; 
+} 
 
-interface route {
-    routeId: number;
-    name: string;
-    url: string;
-}
-
-function NavLink({href, children}: NavLinkProps) {
-    return(
-        <Link href={href} className="relative text-gray-700 hover:text-black font-subheading group transition-colors duration-200">
-            {children}
-            <span className="absolute left-1/2 bottom-0 w-0 h-[2px] bg-black transition-all duration-300 origin-center -translate-x-1/2 group-hover:w-full"/>
-        </Link>
+interface route { 
+    routeId: number; 
+    name: string; 
+    url: string; 
+} 
+function NavLink({href, children}: NavLinkProps) 
+{ return( 
+    <Link href={href} className="relative text-gray-700 hover:text-black font-subheading group transition-colors duration-200">
+        {children}
+        <span className="absolute left-1/2 bottom-0 w-0 h-[2px] bg-black transition-all duration-300 origin-center -translate-x-1/2 group-hover:w-full"/>
+    </Link>
     );
 }
 
