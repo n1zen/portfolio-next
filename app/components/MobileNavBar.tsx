84db3a53c6d9d77f6b72a10dfea49aae4b2e3660
n1zen <114575802+n1zen@users.noreply.github.com>
2026-09-11 21:41:31 +0800

    added white background to Card component

--- app/components/MobileNavBar.tsx
+++ app/components/MobileNavBar.tsx
@@ -9,7 +9,7 @@ export default function MobileNavBar({isMenuOpen, setIsMenuOpen}: MenuProps) {
     return(
         <>
         <Card className={`absolute right-8 mt-2 flex flex-col justify-center items-center
-        transition-all duration-300 origin-top px-4 py-2 bg-white gap-2.5
+        transition-all duration-300 origin-top px-4 py-2 gap-2.5
         ${isMenuOpen
             ? "opacity-100 scale-y-100"
             : "opacity-0 scale-y-0 pointer-events-none"
