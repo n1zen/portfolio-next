6d0bbb393d0322aeb7ffda84f557948616f243b7
n1zenTablet <torresizen070204@gmail.com>
2026-09-15 14:55:55 +0800

    remove right-8 to remove misalignment

--- app/components/MobileNavBar.tsx
+++ app/components/MobileNavBar.tsx
@@ -8,7 +8,7 @@ import SocialLinkButton from "./SocialLinkButton";
 export default function MobileNavBar({isMenuOpen, setIsMenuOpen}: MenuProps) {
     return(
         <>
-        <Card className={`absolute right-8 top-full mt-2 flex flex-col justify-center items-center
+        <Card className={`absolute top-full mt-2 flex flex-col justify-center items-center
         transition-all duration-300 origin-top px-4 py-2 w-full gap-2.5
         ${isMenuOpen
             ? "opacity-100 scale-y-100"
