7c7066a3d5f33a3ad2d9be9a9b39d109353d5096
n1zenTablet <torresizen070204@gmail.com>
2026-09-16 16:13:19 +0800

    Add flex on AboutMe Card as well as the parent home div

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import ProfilePicture from "./components/ProfilePicture";
 
 export default function Home() {
   return (
-    <div>
+    <div className="mt-2 flex flex-col gap-2">
       {/* About me */}
       <Card className="p-2 flex flex-row gap-8">
 	<ProfilePicture /> 
-        <div>
+        <div className="flex flex-col justify-center items-center my-auto">
 	    <h5>About Me</h5>
 	    <p>Lorem ipsum dolor</p>
 	</div>
