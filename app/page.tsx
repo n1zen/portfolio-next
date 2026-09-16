d2f202b85b636496df4ba87212a5dad947efb17d
n1zenTablet <torresizen070204@gmail.com>
2026-09-16 16:50:14 +0800

    fix small grammatical error

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <div className="flex flex-col justify-center items-start my-auto">
 	    <h5 className="font-bold text-xl font-heading">About Me</h5>
 	    <p className="text-xs mt-2">
-		I am an fresh computer engineering graduate who loves turning ideas into working prototypes across web, IoT, and Android. I blend embedded systems, micro-controller programming, and basic networking to build simple, reliable, user-ready apps. I'm looking for a place to learn fast, contribute to real projects, and grow with a supportive team.
+		I am a fresh computer engineering graduate who loves turning ideas into working prototypes across web, IoT, and Android. I blend embedded systems, micro-controller programming, and basic networking to build simple, reliable, user-ready apps. I'm looking for a place to learn fast, contribute to real projects, and grow with a supportive team.
 	    </p>
 	</div>
       </Card>
