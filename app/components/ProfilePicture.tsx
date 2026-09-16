f14070c0e1e8e2255875a12b3b1b1c09e89a4c49
n1zenTablet <torresizen070204@gmail.com>
2026-09-16 15:52:32 +0800

    replaced witdth-full with width-32 and remove object-cover

--- app/components/ProfilePicture.tsx
+++ app/components/ProfilePicture.tsx
@@ -1,5 +1,5 @@
 export default function ProfilePicture() {
     return(
-	<img src="https://scontent.fmnl33-6.fna.fbcdn.net/v/t39.30808-1/736760328_28333776206210347_6163277165224128260_n.jpg?stp=dst-jpg_tt6&cstp=mx1726x1729&ctp=s480x480&_nc_cat=107&ccb=1-7&_nc_sid=e99d92&_nc_eui2=AeF75ONB5L0bj2busnwtjnhFS4Mgr_vEDzRLgyCv-8QPNPlIwB5dcVXOmTUTSr2R7DTGhh8pSs6TMTma068mVdgr&_nc_ohc=lYCHfJPGZekQ7kNvwFSRgL8&_nc_oc=AdoC2NepdkrpoxNPC_kVwAH7YtXs4NlJ8OfQtzXZK7coP9sCeNgILSoiaIWzORjxVkQ&_nc_zt=24&_nc_ht=scontent.fmnl33-6.fna&_nc_gid=7_v2JA_lk7ynlBQpHUfvnA&_nc_ss=7b2a8&oh=00_AQKZZ_tSEDFeP88zWcvtL_Yi3oEdl4xFLfKMS61J68dHIQ&oe=6AB008F4" alt="profile-picture" className="h-32 w-full object-cover rounded-lg"/>
+	<img src="https://scontent.fmnl33-6.fna.fbcdn.net/v/t39.30808-1/736760328_28333776206210347_6163277165224128260_n.jpg?stp=dst-jpg_tt6&cstp=mx1726x1729&ctp=s480x480&_nc_cat=107&ccb=1-7&_nc_sid=e99d92&_nc_eui2=AeF75ONB5L0bj2busnwtjnhFS4Mgr_vEDzRLgyCv-8QPNPlIwB5dcVXOmTUTSr2R7DTGhh8pSs6TMTma068mVdgr&_nc_ohc=lYCHfJPGZekQ7kNvwFSRgL8&_nc_oc=AdoC2NepdkrpoxNPC_kVwAH7YtXs4NlJ8OfQtzXZK7coP9sCeNgILSoiaIWzORjxVkQ&_nc_zt=24&_nc_ht=scontent.fmnl33-6.fna&_nc_gid=7_v2JA_lk7ynlBQpHUfvnA&_nc_ss=7b2a8&oh=00_AQKZZ_tSEDFeP88zWcvtL_Yi3oEdl4xFLfKMS61J68dHIQ&oe=6AB008F4" alt="profile-picture" className="h-32 w-32 rounded-lg"/>
     );
 }
