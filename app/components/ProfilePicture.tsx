69298414ab81049942c789718a265cb2ea2a446a
n1zenTablet <torresizen070204@gmail.com>
2026-09-26 07:59:13 +0800

    move picture to public

--- app/components/ProfilePicture.tsx
+++ app/components/ProfilePicture.tsx
@@ -1,5 +1,5 @@
 export default function ProfilePicture() {
     return(
-	<img src="../assets/profilepic.jpg" alt="profile picture" className="h-32 w-32 rounded-lg" />	
+	<img src="/profilepic.jpg" alt="profile picture" className="h-32 w-32 rounded-lg" />	
     );
 }
