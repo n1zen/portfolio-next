f1ef62f3130f9b3dc4769489988eced38c58223f
n1zenTablet <torresizen070204@gmail.com>
2026-09-26 13:12:46 +0800

    remove duplicate Card component

--- app/page.tsx
+++ app/page.tsx
@@ -18,9 +18,7 @@ export default function Home() {
       </Card>
       <div className="flex md:flex-row flex-col gap-2 justify-center items-center w-full">
         {/* Projects */}
-        <Card className="w-full">
-	    <Projects />
-        </Card>
+        <Projects />
         <div className="flex flex-col gap-4 justify-center items-center w-full">
           {/* Skills */}
           <Card className="w-full">
