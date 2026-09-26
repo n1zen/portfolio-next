111b72215ed32c2082f1ae2a34bf2f4fd97f47b8
n1zenTablet <torresizen070204@gmail.com>
2026-09-26 13:46:49 +0800

    change the font color

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -80,7 +80,7 @@ function ProjectCard({
 	    onTouchEnd={handleTouchEnd}
 	    className={`absolute inset-0 rounded-2xl border border-black shadow-md flex items-center justify-center text-xl font-semibold transition-all duration-300 ease-out ${stackStyles[stackPosition]} ${cardColors[stackPosition]}`}
 	>
-	    {project.title}
+	    <h6 className="text-white">{project.title}</h6>
 	</div>
     );
 }
